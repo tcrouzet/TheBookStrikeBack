a8c2de828d73c329794aafe00cd9b82f789fed36
tcrouzet <tc@tcrouzet.com>
2025-04-25 09:39:40 +0200

    sync 2025-04-25 09:39:40

--- git.sh
+++ git.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #chmod +x git.sh
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
+
+# git fetch origin
+# git diff origin/main
+# git pull origin main
+
 git add .
 git commit -m "sync $current_date"
+git pull origin main
 git push -u origin main
